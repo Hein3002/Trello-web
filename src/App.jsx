8c64425de98c7d0aa8b9c4be76f678a6409f93de
Hein3002 <matcha24200@gmail.com>
2024-06-25 22:25:01 +0700

    Add MUI Theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors';
+import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <>
       <div>hein</div>
+
+      <Typography variant="body2" color="text.secondary">test</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
